ef237262cffe055b4478153df011ed6795c290ab
Marc Gulliver <Marc.gulliver100@gmail.com>
2021-03-17 22:09:16 +0000

    Added sign up template and created a materialize form with styling

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo 
 from bson.objectid import ObjectId 
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -23,6 +24,11 @@ mongo = PyMongo(app)
 def get_tips():
     tips = mongo.db.tips.find()
     return render_template("tips.html", tips=tips)
+
+
+@app.route("/signup", methods=["GET", "POST"])
+def signup():
+    return render_template("signup.html")
 
 
 if __name__ == "__main__":
