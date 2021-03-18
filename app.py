2c6aa88265551ff4e6da5772e8c420a89409cb7b
Marc Gulliver <Marc.gulliver100@gmail.com>
2021-03-18 16:54:23 +0000

    Added user profile page which is accessed through logging in

--- app.py
+++ app.py
@@ -44,6 +44,8 @@ def signup():
 
         session["user"] = request.form.get("username").lower()
         flash("Signup Successful")
+        return redirect(url_for("profile", username=session["user"]))
+
     return render_template("signup.html")
 
 
@@ -57,7 +59,10 @@ def login():
             if check_password_hash(
                 existing_user["password"], request.form.get("password")):
                     session["user"] = request.form.get("username").lower()
-                    flash("Welcome, {}".format(request.form.get("username")))
+                    flash("Welcome, {}".format(
+                        request.form.get("username")))
+                    return redirect(url_for(
+                        "profile", username=session["user"]))
             else:
                 flash("Incorrect Username or Password")
                 return redirect(url_for("login"))
@@ -67,6 +72,13 @@ def login():
             return redirect(url_for("login"))
 
     return render_template("login.html")
+
+
+@app.route("/profile/<username>", methods=["GET", "POST"])
+def profile(username):
+    username = mongo.db.users.find_one(
+        {"username": session["user"]})["username"]
+    return render_template("profile.html", username=username)
 
 
 if __name__ == "__main__":
