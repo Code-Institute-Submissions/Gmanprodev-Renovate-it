a9497a8e3d9238ad3eb0f658d7f39e404ac6738b
Marc Gulliver <Marc.gulliver100@gmail.com>
2021-03-31 11:09:52 +0000

    Linked up the dropdown selection in add tips page with mongodb database

--- app.py
+++ app.py
@@ -99,7 +99,8 @@ def logout():
 
 @app.route("/add_tips")
 def add_tips():
-    return render_template("add_tips.html")
+    categories = mongo.db.categories.find().sort("category_name, 1")
+    return render_template("add_tips.html", categories=categories)
 
 
 if __name__ == "__main__":
