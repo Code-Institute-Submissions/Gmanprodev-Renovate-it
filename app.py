ffab346740fdf5e9abb1833bb116817e99d03437
Marc Gulliver <Marc.gulliver100@gmail.com>
2021-04-30 11:41:49 +0000

    Reversed changes to earlier commit

--- app.py
+++ app.py
@@ -28,7 +28,8 @@ def index():
 @app.route("/get_tips")
 def get_tips():
     tips = list(mongo.db.tips.find())
-    return render_template("tips.html", tips=tips)
+    categories = list(mongo.db.categories.find())
+    return render_template("tips.html", tips=tips, categories=categories)
 
 
 @app.route("/search", methods=["GET", "POST"])
@@ -90,10 +91,12 @@ def profile(username):
     username = mongo.db.users.find_one(
         {"username": session["user"]})["username"]
     tips = list(mongo.db.tips.find({"username": session["user"]}))
+    categories = list(mongo.db.categories.find())
     if session["user"]:
-        return render_template("profile.html", username=username, tips=tips)
-
-    return redirect(url_for("login"))
+        return render_template("profile.html", username=username, tips=tips,
+            categories=categories)
+    else:
+        return redirect(url_for("login"))
 
 
 @app.route("/logout")
